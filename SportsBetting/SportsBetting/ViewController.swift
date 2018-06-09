0864747b7842c1c433ff07f0be9f4f7155a1d438
Tarun Singh <tarunsingh@WTA-MBP72.local>
2018-06-09 16:31:24 -0400

    Intitilized swift files

---
+++ SportsBetting/SportsBetting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SportsBetting
+//
+//  Created by Tarun Singh on 6/9/18.
+//  Copyright © 2018 Tarun Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
