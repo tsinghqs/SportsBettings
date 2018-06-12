6a05a5476b689c35684fa698560a342ed93ce70f
Soham Dessai <sd5we@virginia.edu>
2018-06-12 19:16:13 -0400

    Hype Calculator

--- SportsBetting/SportsBetting/ViewController.swift
+++ SportsBetting/SportsBetting/ViewController.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var oddsText: UITextField!
-    @IBOutlet weak var amountText: UITextField!
+    
+    
     var plusminus = true
-    var payout = 0
-    @IBAction func indexChanged(_ sender: AnyObject) {
-        switch sender.selectedSegmentIndex
+    var payout = 0.0
+   
+    @IBAction func indexChanged(_ sender: Any) {
+        switch (sender as AnyObject).selectedSegmentIndex
         {
         case 0:
-           plusminus = true
+            plusminus = true
         case 1:
             plusminus = false
         default:
@@ -26,22 +27,30 @@ class ViewController: UIViewController {
         }
     }
     
+    
+    @IBOutlet weak var amountText: UITextField!
+    @IBOutlet weak var oddsText: UITextField!
     @IBOutlet weak var payoutText: UILabel!
-    @IBAction func Calculate(_ sender: AnyObject) {
-         var odds  = Int(oddsText.text!)
-         var amount = Int(amountText.text!)
-        
-        if plusminus {
-            payout = (amount!/odds!) * 100 + amount!
-            payoutText.text = String(payout)
+    
+    
+    @IBAction func Calculate(_ sender: Any) {
+        if let odds = Double (oddsText.text!), let amount = Double (amountText.text!) {
+            if plusminus {
+                payout = (amount/odds) * 100 + amount
+                payoutText?.text = String(payout)
+            }
+            else {
+                payout = (amount/100) * odds  + amount
+                payoutText?.text = String(payout)
+            }
+    }
+        else{
+            payoutText.text = "false"
         }
-        else {
-            payout = (amount!/100) * odds!  + amount!
-            payoutText.text = String(payout)
-        }
-        
         
     }
+    
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
