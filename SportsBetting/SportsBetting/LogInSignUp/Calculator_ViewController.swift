e31a376036388c5e1717bad13516e7bc61e2b122
Soham Dessai <sd5we@virginia.edu>
2018-06-13 16:53:26 -0400

    font

---
+++ SportsBetting/SportsBetting/LogInSignUp/Calculator_ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  Calculator_ViewController.swift
+//  SportsBetting
+//
+//  Created by Soham Dessai on 6/13/18.
+//  Copyright © 2018 Tarun Singh. All rights reserved.
+//
+
+import UIKit
+
+class Calculator_ViewController: UIViewController {
+
+   
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
