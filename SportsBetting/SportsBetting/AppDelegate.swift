293d6baa5de985473f9ed7e755d6d54bcb2f53cc
tsinghqs@vt.edu <tsinghqs@vt.edu>
2018-06-14 16:22:41 -0400

    cleaned project file

--- SportsBetting/SportsBetting/AppDelegate.swift
+++ SportsBetting/SportsBetting/AppDelegate.swift
@@ -8,36 +8,12 @@
 
 import UIKit
 
-//  AppDelegate.m
-//import <FBSDKCoreKit/FBSDKCoreKit.h>
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    /**
-    (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    
-    [[FBSDKApplicationDelegate sharedInstance] application:application
-    didFinishLaunchingWithOptions:launchOptions];
-    // Add any custom logic here.
-    return YES;
-    }
-    
-    (BOOL)application:(UIApplication *)application
-    openURL:(NSURL *)url
-    options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    
-    BOOL handled = [[FBSDKApplicationDelegate sharedInstance] application:application
-    openURL:url
-    sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]
-    ];
-    // Add any custom logic here.
-    return handled;
-    }
- **/
+
     var window: UIWindow?
 
 
@@ -47,17 +23,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+       
+        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
--- SportsBetting/SportsBetting/ViewController.swift
+++ SportsBetting/SportsBetting/ViewController.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import FacebookLogin
 
 
 class ViewController: UIViewController {
